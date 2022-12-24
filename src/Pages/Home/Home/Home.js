d731e9d5125038b06bd87a5c6a3a58c8c63af1b0
tasfi60 <tasfiagirl@gmail.com>
2022-12-24 23:15:12 +0600

    products updated & motto section added from tasfi

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Motto from '../Motto/Motto';
 import Products from '../Products/Products';
 
 const Home = () => {
     return (
     <div className>
         <Banner></Banner>
+        <Motto></Motto>
         <Products></Products>
     </div>
     );
