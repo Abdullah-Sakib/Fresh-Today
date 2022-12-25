9cadd9d5bafe1ef823710b41a42a0e4cdcd8d164
Ankan112 <ankanhalder112@gmail.com>
2022-12-25 12:56:01 +0600

    login page image update

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
   }
   return (
     <form onSubmit={handleLoginForm} className="flex w-full max-w-sm mx-auto overflow-hidden my-10 bg-white rounded-lg shadow-lg dark:bg-gray-800 lg:max-w-4xl">
-      <div className="hidden bg-cover lg:block lg:w-1/2" style={{ backgroundImage: `url('https://images.unsplash.com/photo-1606660265514-358ebbadc80d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1575&q=80')` }}></div>
+      <div className="hidden bg-cover lg:block lg:w-1/2" style={{ backgroundImage: `url('https://cdn.pixabay.com/photo/2017/01/29/16/17/ornament-2018617_960_720.png')` }}></div>
 
       <div className="w-full px-6 py-8 md:px-8 lg:w-1/2">
         <h2 className="text-2xl font-semibold text-center text-gray-700 dark:text-white">
