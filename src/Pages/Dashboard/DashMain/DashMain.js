85295cb73e28b0251173d1fd62d5aac26338bf5d
Abdullah-Sakib <abdullahsakib888@gmail.com>
2022-12-25 09:44:39 +0600

    Added sidebar open button in dashboard

--- src/Pages/Dashboard/DashMain/DashMain.js
+++ src/Pages/Dashboard/DashMain/DashMain.js
@@ -11,28 +11,27 @@ const DashMain = () => {
           <div className="drawer-content flex flex-col p-5">
             {/* <!-- Page content here --> */}
             <Outlet></Outlet>
-            <label
-              htmlFor="my-drawer-2"
-              className="btn btn-primary drawer-button lg:hidden"
-            >
-              Open drawer
-            </label>
           </div>
         </div>
         <div className="drawer-side">
           <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-          <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+          <ul className="menu p-4 w-80 bg-base-200 text-base-content ">
             {/* <!-- Sidebar content here --> */}
+
             <li>
+            <Link to="/">Logo will be here</Link>
+            </li>
+
+            <li className="text-lg font-semibold">
               <Link to="/dashboard/users">Users</Link>
             </li>
-            <li>
+            <li className="text-lg font-semibold"> 
               <Link to="/dashboard/products">Products</Link>
             </li>
-            <li>
+            <li className="text-lg font-semibold">
               <Link to="/dashboard/cart">Cart</Link>
             </li>
-            <li>
+            <li className="text-lg font-semibold">
               <Link to="/dashboard/reports">Reports</Link>
             </li>
           </ul>
--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -4,6 +4,12 @@ const Dashboard = () => {
     return (
         <div>
             <h3>This is dashboard</h3>
+            <label
+              htmlFor="my-drawer-2"
+              className="btn btn-primary drawer-button lg:hidden"
+            >
+              Open drawer
+            </label>
         </div>
     );
 };
