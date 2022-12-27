41ee64fb5bc9542e86f07d658dfc01a49050476d
Abdullah-Sakib <abdullahsakib888@gmail.com>
2022-12-27 18:27:23 +0600

    Hosting product image in imgbb server

--- src/Pages/Home/TopRatedVendors/VendorCard.js
+++ src/Pages/Home/TopRatedVendors/VendorCard.js
@@ -1,30 +1,72 @@
-import React from 'react';
+import React from "react";
 
-const VendorCard = ({vendor}) => {
-    const {name, image, orders} = vendor;
+const VendorCard = ({ vendor }) => {
+  const { name, image, orders } = vendor;
 
-    return (
-        <div className="card card-side bg-slate-50 shadow-2xl h-40 mx-2 w-full">
-            <figure className='w-1/2 lg:w-1/4'><img className='h-full' src={image} alt={name}/></figure>
-            <div className="card-body">
-                <h2 className="card-title">{name}</h2>
-                <p>Orders completed: {orders}</p>
-                <div className="rating rating-lg rating-half">
-                    <input type="radio" name="rating-10" className="rating-hidden" />
-                    <input type="radio" name="rating-10" className="bg-green-500 mask mask-star-2 mask-half-1" />
-                    <input type="radio" name="rating-10" className="bg-green-500 mask mask-star-2 mask-half-2" />
-                    <input type="radio" name="rating-10" className="bg-green-500 mask mask-star-2 mask-half-1" checked />
-                    <input type="radio" name="rating-10" className="bg-green-500 mask mask-star-2 mask-half-2" />
-                    <input type="radio" name="rating-10" className="bg-green-500 mask mask-star-2 mask-half-1" />
-                    <input type="radio" name="rating-10" className="bg-green-500 mask mask-star-2 mask-half-2" />
-                    <input type="radio" name="rating-10" className="bg-green-500 mask mask-star-2 mask-half-1" />
-                    <input type="radio" name="rating-10" className="bg-green-500 mask mask-star-2 mask-half-2" />
-                    <input type="radio" name="rating-10" className="bg-green-500 mask mask-star-2 mask-half-1" />
-                    <input type="radio" name="rating-10" className="bg-green-500 mask mask-star-2 mask-half-2" />
-                </div>
-            </div>
+  return (
+    <div className="card card-side bg-slate-50 shadow-2xl h-40 mx-2 w-full">
+      <figure className="w-1/2 lg:w-1/4">
+        <img className="h-full" src={image} alt={name} />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{name}</h2>
+        <p>Orders completed: {orders}</p>
+        <div className="rating rating-lg rating-half">
+          <input type="radio" name="rating-10" className="rating-hidden" />
+          <input
+            type="radio"
+            name="rating-9"
+            className="bg-green-500 mask mask-star-2 mask-half-1"
+          />
+          <input
+            type="radio"
+            name="rating-10"
+            className="bg-green-500 mask mask-star-2 mask-half-2"
+          />
+          <input
+            type="radio"
+            name="rating-10"
+            className="bg-green-500 mask mask-star-2 mask-half-1"
+          />
+          <input
+            type="radio"
+            name="rating-10"
+            className="bg-green-500 mask mask-star-2 mask-half-2"
+          />
+          <input
+            type="radio"
+            name="rating-10"
+            className="bg-green-500 mask mask-star-2 mask-half-1"
+          />
+          <input
+            type="radio"
+            name="rating-10"
+            className="bg-green-500 mask mask-star-2 mask-half-2"
+          />
+          <input
+            type="radio"
+            name="rating-10"
+            className="bg-green-500 mask mask-star-2 mask-half-1"
+          />
+          <input
+            type="radio"
+            name="rating-10"
+            className="bg-green-500 mask mask-star-2 mask-half-2"
+          />
+          <input
+            type="radio"
+            name="rating-10"
+            className="bg-green-500 mask mask-star-2 mask-half-1"
+          />
+          <input
+            type="radio"
+            name="rating-10"
+            className="bg-green-500 mask mask-star-2 mask-half-2"
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default VendorCard;
