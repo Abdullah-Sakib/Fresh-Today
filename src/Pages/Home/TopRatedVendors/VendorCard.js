8f693d7bd93d2acde57404d7a11ea52a2f1deae5
Akil Chowdhury <akiluddinchy@gmail.com>
2022-12-31 13:37:09 +0600

    ratings added in top vendors section

--- src/Pages/Home/TopRatedVendors/VendorCard.js
+++ src/Pages/Home/TopRatedVendors/VendorCard.js
@@ -11,59 +11,13 @@ const VendorCard = ({ vendor }) => {
       <div className="card-body">
         <h2 className="card-title">{name}</h2>
         <p>Orders completed: {orders}</p>
-        <div className="rating rating-lg rating-half">
-          <input type="radio" name="rating-10" className="rating-hidden" />
-          <input
-            type="radio"
-            name="rating-9"
-            className="bg-green-500 mask mask-star-2 mask-half-1"
-          />
-          <input
-            type="radio"
-            name="rating-10"
-            className="bg-green-500 mask mask-star-2 mask-half-2"
-          />
-          <input
-            type="radio"
-            name="rating-10"
-            className="bg-green-500 mask mask-star-2 mask-half-1"
-          />
-          <input
-            type="radio"
-            name="rating-10"
-            className="bg-green-500 mask mask-star-2 mask-half-2"
-          />
-          <input
-            type="radio"
-            name="rating-10"
-            className="bg-green-500 mask mask-star-2 mask-half-1"
-          />
-          <input
-            type="radio"
-            name="rating-10"
-            className="bg-green-500 mask mask-star-2 mask-half-2"
-          />
-          <input
-            type="radio"
-            name="rating-10"
-            className="bg-green-500 mask mask-star-2 mask-half-1"
-          />
-          <input
-            type="radio"
-            name="rating-10"
-            className="bg-green-500 mask mask-star-2 mask-half-2"
-          />
-          <input
-            type="radio"
-            name="rating-10"
-            className="bg-green-500 mask mask-star-2 mask-half-1"
-          />
-          <input
-            type="radio"
-            name="rating-10"
-            className="bg-green-500 mask mask-star-2 mask-half-2"
-          />
-        </div>
+        <div className="rating">
+          <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
+          <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
+          <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
+          <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
+          <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
+      </div>
       </div>
     </div>
   );
