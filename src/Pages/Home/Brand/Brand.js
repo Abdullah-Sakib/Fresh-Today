cc41360447ba3e8175a90f447611b4b47dbfac4f
tasfi60 <tasfiagirl@gmail.com>
2023-01-02 14:51:34 +0600

    home updated

---
+++ src/Pages/Home/Brand/Brand.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+const Brand = () => {
+    return (
+        <div>
+
+<h1 className="pb-10 text-center font-bold text-slate-800 text-5xl font-family my-20">
+        Popular Brand
+      </h1>
+        <div className='flex items-center justify-center font-family '>
+           
+            <div className='grid grid-cols-1 gap-28 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-8 xl:grid-cols-8'>
+
+            
+            <div class="h-44 w-32  rounded-xl flex flex-col justify-center  duration-300 hover:bg-white hover:shadow-xl">
+    <img src="https://chefcart.com.bd/wp-content/uploads/2020/01/nestle.jpg" alt="" />
+  
+</div>
+            <div class="h-44 w-32 rounded-xl flex flex-col justify-center  duration-300 hover:bg-white hover:shadow-xl">
+    <img src="https://chefcart.com.bd/wp-content/uploads/2020/01/HERSHEYs.jpg" alt="" />
+  
+</div>
+            <div class="h-44 w-32 rounded-xl flex flex-col justify-center  duration-300 hover:bg-white hover:shadow-xl">
+    <img src="https://chefcart.com.bd/wp-content/uploads/2020/09/Carnation-brand.jpg" alt="" />
+  
+</div>
+            <div class="h-44 w-32  rounded-xl flex flex-col justify-center  duration-300 hover:bg-white hover:shadow-xl">
+    <img src="https://chefcart.com.bd/wp-content/uploads/2020/09/aarong-brand.jpg" alt="" />
+  
+</div>
+            <div class="h-44 w-32  rounded-xl flex flex-col justify-center  duration-300 hover:bg-white hover:shadow-xl">
+    <img src="https://chefcart.com.bd/wp-content/uploads/2020/09/Bruggeman-brand.jpg" alt="" />
+  
+</div>
+            <div class="h-44 w-32 rounded-xl flex flex-col justify-center  duration-300 hover:bg-white hover:shadow-xl">
+   <img src="https://chefcart.com.bd/wp-content/uploads/2020/09/bragg-brand.jpg" alt="" />
+  
+</div>
+            <div class="h-44 w-32  rounded-xl flex flex-col justify-center  duration-300 hover:bg-white hover:shadow-xl">
+    <img src="https://chefcart.com.bd/wp-content/uploads/2020/09/amul-brand.jpg" alt="" />
+  
+</div>
+            <div class="h-44 w-32  rounded-xl flex flex-col justify-center  duration-300 hover:bg-white hover:shadow-xl">
+   <img src="https://chefcart.com.bd/wp-content/uploads/2020/01/Ahmed.jpg" alt="" />
+</div>
+        </div>
+        </div>
+        </div>
+    );
+};
+
+export default Brand;
